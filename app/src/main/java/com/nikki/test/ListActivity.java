89b3ca9d543c24addc91fa1319f024eb5693f67c
Nikhitha <nickytarala@gmail.com>
2017-03-21 15:38:30 -0500

    List Activity

---
+++ app/src/main/java/com/nikki/test/ListActivity.java
@@ -0,0 +1,18 @@
+package com.nikki.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nikhitha Tarala on 3/21/2017.
+ */
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
